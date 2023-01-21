d3518f05b424744e1557f8ea72c82b62a1eadda9
Dubu Devs <dylan.h.owens@icloud.com>
2023-01-21 21:28:06 +0000

    .

--- getplayerdata.py
+++ getplayerdata.py
@@ -4,14 +4,15 @@ import bs4
 import secretfile
 
 sess = secretfile.sess
+id = secretfile.id
 
 def getquests(id):
-    headers = {f"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0",
-"Cookie": "ldst_touchstone=1; ldst_is_support_browser=1; ldst_sess={sess}; platform_mode=win"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0",
+"Cookie": f"ldst_touchstone=1; ldst_is_support_browser=1; ldst_sess={sess}; platform_mode=win"
 }
-    page1 = requests.get("https://eu.finalfantasyxiv.com/lodestone/character/{id}/quest/", headers=headers).text
+    page1 = requests.get(f"https://eu.finalfantasyxiv.com/lodestone/character/{id}/quest/", headers=headers).text
     page1soup = bs4.BeautifulSoup(page1)
     print(page1soup.prettify())
     print(page1soup.select_one('a[class*="btn__pager__next--all"]'))
 
-getquests(secretfile.id)
+getquests(id)
